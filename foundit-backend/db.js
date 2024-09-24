6cfd2df48bee0197d4aa5f3e26095f9e73745de3
YashChoudhary <yashchoudhary572001@gmail.com>
2024-09-24 23:17:00 +0530

    new changes

--- foundit-backend/db.js
+++ foundit-backend/db.js
@@ -2,7 +2,9 @@ const mongoose=require('mongoose');
 
 require('dotenv').config();
 
-const mongoURI = `mongodb://localhost:27017/Found_It`;
+// const mongoURI = `mongodb://localhost:27017/Found_It`;
+const mongoURI = `mongodb+srv://yashchoudhary572001:2ONDs5kzRSWUaO35@cluster0.m6uqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 const connectToMongo=()=>{
   mongoose.connect(mongoURI, {
